ebce8464469e74ee2e50b6ef4ce6aba1fe66726d
TheJamesBattye <Jamesbattye98@gmail.com>
2023-07-19 11:31:31 +0100

    added move items util & slider mobile script

--- src/slider.ts
+++ src/slider.ts
@@ -3,44 +3,38 @@
 // If you're getting red lines under the next 2 lines, write this in your terminal 'sudo pnpm i' 
 // then write 'pnpm install swiper' in the terminal after that.
 import { Swiper } from 'swiper';
-import 'swiper/swiper-bundle.css';
-
-function getFirstWord(str: string): string {
-  // Trim any leading or trailing spaces from the string
-  str = str.trim();
-  // Find the first space in the string
-  const spaceIndex = str.indexOf(' ');
-  if (spaceIndex === -1) {
-    // If there are no spaces in the string, return the whole string
-    return str;
-  } else {
-    // If there is a space in the string, return the substring before the space
-    return str.substring(0, spaceIndex);
-  }
-}
+import { Navigation, Pagination } from 'swiper/modules';
+import { getFirstWord } from 'src/utils/getClassName';
 
 const sliders = document.querySelectorAll<HTMLElement>("[element='slider-component']");
 
 sliders.forEach((e) => {
-  console.log(e);
   const wrapper = e.querySelector<HTMLElement>("[element='wrapper']");
   const list = e.querySelector<HTMLElement>("[element='list']");
   const item = e.querySelector<HTMLElement>("[element='item']");
   const nav = e.querySelector<HTMLElement>("[element='navigation']");
+  const paginationElement = e.querySelector<HTMLElement>("[element='pagination']")
   const nextArrow = nav!.querySelector<HTMLElement>("[element='next-arrow']");
   const prevArrow = nav!.querySelector<HTMLElement>("[element='prev-arrow']");
+  const paginationDot = paginationElement?.querySelector<HTMLElement>("[element='pagination-dot']")
+  const paginationActiveClass = 'is-active'
+
 
   const listRaw = list!.className;
   const itemRaw = item!.className;
+  const paginationDotRaw = paginationDot!.className;
 
   const listClass = getFirstWord(listRaw);
   const itemClass = getFirstWord(itemRaw);
+  const paginationDotClass = getFirstWord(paginationDotRaw)
 
   if (wrapper) {
     const swiper = new Swiper(wrapper, {
+      modules: [Navigation, Pagination],
       speed: 400,
-      spaceBetween: 16,
-      slidesPerView: 3,
+      spaceBetween: 0,
+      slidesPerView: 1,
+      loop: true,
       direction: 'horizontal',
       wrapperClass: listClass!,
       slideClass: itemClass!,
@@ -48,12 +42,13 @@ sliders.forEach((e) => {
         nextEl: nextArrow,
         prevEl: prevArrow,
       },
-    });
-    nextArrow?.addEventListener('click', () => {
-      swiper.slideNext();
-    });
-    prevArrow?.addEventListener('click', () => {
-      swiper.slidePrev();
+      pagination: {
+        el: paginationElement,
+        bulletActiveClass: paginationActiveClass,
+        bulletClass: paginationDotClass,
+        bulletElement: "button",
+        clickable: true
+      }
     });
   }
 });
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+import { swap } from "src/utils/moveItems";
+
+const sort = document.querySelector('.cs_filter_sort-dropdown')
+const newLocation = document.querySelector("[element='appender']")
+
+
+swap(767, sort, newLocation)
+
